08e69382263124e8ebe731fdf83bc77afd5cac9a
michael-mamman <michaelmamman85@gmail.com>
2022-03-02 03:07:14 -0800

    fixed new bugs

--- employee/urls.py
+++ employee/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from employee import views
 
-# final review of route and controller
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('emp', views.emp),
