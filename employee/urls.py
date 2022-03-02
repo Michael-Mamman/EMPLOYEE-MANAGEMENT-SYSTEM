dba1b091522e9c54491c026a2107a458fa93a65f
Annastasia-dev <78227740+Annastasia-dev@users.noreply.github.com>
2022-03-02 11:31:14 +0100

    Review and added routes
    the routes where incomplete

--- employee/urls.py
+++ employee/urls.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 from django.urls import path
 from employee import views
 
-
+# final review of route and controller
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('emp', views.emp),
+    path('show', views.show),
+    path('edit/<int:id>', views.edit),
+    path('update/<int:id>', views.update),
+    path('delete/<int:id>', views.destroy)
+]
