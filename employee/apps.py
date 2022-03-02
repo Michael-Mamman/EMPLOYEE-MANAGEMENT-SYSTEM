cd7c04af015c0daeaa0cbc915c91088b823265d6
Emmanuel-Longkang <99477707+Emmanuel-Longkang@users.noreply.github.com>
2022-03-02 11:38:37 +0100

    Fixing Bugs and adding a comment
    read comment above

--- employee/apps.py
+++ employee/apps.py
@@ -1,4 +1,7 @@
 from django.apps import AppConfig
 
-
+#connceting created application project 
+class EmployeeConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'employee'
 
